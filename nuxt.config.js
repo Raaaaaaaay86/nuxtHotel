f36ef375bd057bb8a158f16cd12c45308e60932f
Raaaaaaaay86 <mirror46258@gmail.com>
2021-02-06 09:52:28 +0800

    更換 icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'hotel',
+    title: '好室 Hotel',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
